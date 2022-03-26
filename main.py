e3055c505f161c403552396e72a2cda900a104ce
appletime81 <itrimsl8696@gmail.com>
2022-03-26 10:00:53 +0800

    Update

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from pprint import pprint
 def read_Hyde_EMS(file_name='Hyde_EMS.xlsx'):
     hydes_ems_df = pd.read_excel(file_name)
     hydes_ems_df = hydes_ems_df.sort_values(by=['MACHINE_ID', 'START_DATE'])
-    hydes_ems_df.to_excel('test.xlsx', index=False)
+    return hydes_ems_df
+    # hydes_ems_df.to_excel('test.xlsx', index=False)
 
 
 if __name__ == '__main__':
-    read_Hyde_EMS()
+    hydes_ems_df = read_Hyde_EMS()
